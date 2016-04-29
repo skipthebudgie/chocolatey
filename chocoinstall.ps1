edd7afea4547149768963c1ce1344be1be4a881b
skipthebudgie <daveashby@gmail.com>
2016-04-29 20:51:12 +0100

    Create chocoinstall.ps1

---
+++ chocoinstall.ps1
@@ -0,0 +1,44 @@
+# Chocolatey
+iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+ 
+# install applications
+# Media:
+cinst vlc
+cinst mediamonkey
+cinst kodi
+cinst utorrent
+cinst mediainfo
+cinst filebot
+
+# Tekky
+cinst putty
+cinst sysinternals
+cinst fiddler
+cinst windirstat
+cinst 7zip
+cinst onedrive
+cinst googledrive
+cinst cygwin
+
+
+# Core software
+cinst notepadplusplus
+cinst filezilla
+cinst googleChrome
+# cinst Autohotkey
+cinst adobe-creative-cloud
+cinst skype
+# cinst handbrake
+# cinst imgburn
+# cinst anydvd
+
+# Antivirus & cleanup
+cinst Avastfreeantivirus
+cinst Malwarebytes
+cinst Ccleaner
+
+# Extras
+cinst jre8
+cinst Flash
+cinst Silverlight
+cinst Dotnet4.5
